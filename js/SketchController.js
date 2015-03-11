b9375b454cbe9ee87c7ae406bd7f7d41a77e36d0
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-03-11 13:00:18 -0400

    Shim for bind

--- js/SketchController.js
+++ js/SketchController.js
@@ -1,3 +1,46 @@
+var shim = function(){
+    console.log("in shim");
+        var isFunction = function(o) {
+          return typeof o == 'function';
+        };
+
+        var bind,
+          slice = [].slice,
+          proto = Function.prototype,
+          featureMap;
+
+        featureMap = {
+          'function-bind': 'bind'
+        };
+
+        function has(feature) {
+          var prop = featureMap[feature];
+          return isFunction(proto[prop]);
+        }
+
+        // check for missing features
+	console.log('before bind');
+        if (!has('function-bind')) {
+          // adapted from Mozilla Developer Network example at
+          // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+          bind = function bind(obj) {
+            console.log('in bind');
+            var args = slice.call(arguments, 1),
+              self = this,
+              nop = function() {
+              },
+              bound = function() {
+                return self.apply(this instanceof nop ? this : (obj || {}), args.concat(slice.call(arguments)));
+              };
+            nop.prototype = this.prototype || {}; // Firefox cries sometimes if prototype is undefined
+            bound.prototype = new nop();
+            return bound;
+          };
+          proto.bind = bind;
+        }
+};
+shim();
+
 define([
   'angular',
   'leap',
