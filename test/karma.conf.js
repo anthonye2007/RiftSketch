a272e73604045e6388a59c592fcf30310c79d931
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-04-20 10:18:44 -0400

    Add Firefox as browser

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -65,7 +65,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['PhantomJS', '/c/bin/Firefox/custom-firefox.sh', 'Firefox'],
 
 
     // Continuous Integration mode
